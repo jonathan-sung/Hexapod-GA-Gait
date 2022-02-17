cf0d78bdb3ba7ddd813ca0e6ed8d2d68061d8f07
jonathan-sung <60101351+jonathan-sung@users.noreply.github.com>
2022-02-17 12:49:03 +0000

    GA integration "nan" bug fix reset simulation

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 l1 = [x for x in list(range(0, 24)) if x not in list(range(3, 24, 4))]
 l2 = [x for x in range(0, 24) if (x + 1) % 4 != 0]
@@ -8,3 +9,8 @@ print(l2)
 print(len(l2))
 cheese = random.uniform(0, 0)
 print(cheese)
+
+x = float('nan')
+print(math.isnan(x))
+print(x)
+
