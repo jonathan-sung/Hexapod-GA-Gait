5dea9bc2f3a08980b9e69c9cbf13c2973c42e98e
jonathan-sung <60101351+jonathan-sung@users.noreply.github.com>
2022-02-25 18:57:24 +0000

    integration of 4 fitness parameters into one fitness value

--- evolve_cyclic_gait.py
+++ evolve_cyclic_gait.py
@@ -16,7 +16,7 @@ DIMENSIONS = 2 + h.LENGTH_OF_GAIT_STATE * (h.LENGTH_OF_SEQUENCE - 1)
 BOUNDS_LOW = [0] + [140] + ([0] + ([-h.servoRangeOfMotion] * h.NUM_OF_SERVOS)) * (h.LENGTH_OF_SEQUENCE - 1)
 BOUNDS_HIGH = [1] + [160] + ([1] + ([h.servoRangeOfMotion] * h.NUM_OF_SERVOS)) * (h.LENGTH_OF_SEQUENCE - 1)
 
-POPULATION_SIZE = 1000
+POPULATION_SIZE = 100
 P_CROSSOVER = 0.9  # probability for crossover
 P_MUTATION = 0.5  # probability for mutating an individual
 MAX_GENERATIONS = 100
@@ -34,7 +34,7 @@ print(f"Estimated Evolution Time: {estimatedTime} hours")
 print("GA Parameters", POPULATION_SIZE, P_CROSSOVER, P_MUTATION, MAX_GENERATIONS, HALL_OF_FAME_SIZE, CROWDING_FACTOR, DIMENSIONS, TOURN_SIZE, MUT_INDPB, DISTANCE_WEIGHT, ANGLE_WEIGHT, STABILITY_WEIGHT)
 
 toolbox = base.Toolbox()
-creator.create("FitnessCompound", base.Fitness, weights=(DISTANCE_WEIGHT, ANGLE_WEIGHT, STABILITY_WEIGHT, HEIGHT_WEIGHT))
+creator.create("FitnessCompound", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessCompound)
 
 # creator.create("FitnessSingle", base.Fitness, weights=(DISTANCE_WEIGHT,))
@@ -51,8 +51,8 @@ for i in range(DIMENSIONS):
 toolbox.register("individualCreator", tools.initCycle, creator.Individual, layer_size_attributes, n=1)
 toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
 toolbox.register("evaluate", h.evaluateGait)
-# toolbox.register("select", tools.selTournament, tournsize=TOURN_SIZE)
-toolbox.register("select", tools.selNSGA2)
+toolbox.register("select", tools.selTournament, tournsize=TOURN_SIZE)
+# toolbox.register("select", tools.selNSGA2)
 toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUNDS_LOW, up=BOUNDS_HIGH, eta=CROWDING_FACTOR)
 toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUNDS_LOW, up=BOUNDS_HIGH, eta=CROWDING_FACTOR, indpb=MUT_INDPB)
 
@@ -67,6 +67,7 @@ def main():
     stats.register("max", np.max)
     stats.register("avg", np.mean)
     hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
+    # hof = tools.ParetoFront()
     population, logbook = elitism.eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION, ngen=MAX_GENERATIONS, stats=stats, halloffame=hof, verbose=True)
     # population, logbook = algorithms.eaSimple(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION, ngen=MAX_GENERATIONS, stats=stats, halloffame=hof, verbose=True)
     best = hof.items[0]
