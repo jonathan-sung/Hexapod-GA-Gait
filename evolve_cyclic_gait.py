6bace4c81c81231ec4e8680af3397df7eaed9bda
jonathan-sung <60101351+jonathan-sung@users.noreply.github.com>
2022-02-25 16:16:40 +0000

    stability and height fitness added

--- evolve_cyclic_gait.py
+++ evolve_cyclic_gait.py
@@ -9,24 +9,36 @@ import matplotlib.pyplot as plt
 import hexapodengine2 as h
 import elitism
 import multiprocessing
+import math
+import time
 
 DIMENSIONS = 2 + h.LENGTH_OF_GAIT_STATE * (h.LENGTH_OF_SEQUENCE - 1)
 BOUNDS_LOW = [0] + [140] + ([0] + ([-h.servoRangeOfMotion] * h.NUM_OF_SERVOS)) * (h.LENGTH_OF_SEQUENCE - 1)
 BOUNDS_HIGH = [1] + [160] + ([1] + ([h.servoRangeOfMotion] * h.NUM_OF_SERVOS)) * (h.LENGTH_OF_SEQUENCE - 1)
 
-POPULATION_SIZE = 3000
+POPULATION_SIZE = 1000
 P_CROSSOVER = 0.9  # probability for crossover
 P_MUTATION = 0.5  # probability for mutating an individual
-MAX_GENERATIONS = 1000
-HALL_OF_FAME_SIZE = 30
+MAX_GENERATIONS = 100
+HALL_OF_FAME_SIZE = int(0.1 * POPULATION_SIZE)
 CROWDING_FACTOR = 20.0  # crowding factor for crossover and mutation
 TOURN_SIZE = 2
 MUT_INDPB = 1.0 / DIMENSIONS
-print("GA Parameters", POPULATION_SIZE, P_CROSSOVER, P_MUTATION, MAX_GENERATIONS, HALL_OF_FAME_SIZE, CROWDING_FACTOR, DIMENSIONS, TOURN_SIZE, MUT_INDPB)
+DISTANCE_WEIGHT = 1.0
+ANGLE_WEIGHT = -1.0
+STABILITY_WEIGHT = 1.0
+HEIGHT_WEIGHT = -1.0
+
+estimatedTime = (0.115 * POPULATION_SIZE * MAX_GENERATIONS) / (60.0 * 60.0)
+print(f"Estimated Evolution Time: {estimatedTime} hours")
+print("GA Parameters", POPULATION_SIZE, P_CROSSOVER, P_MUTATION, MAX_GENERATIONS, HALL_OF_FAME_SIZE, CROWDING_FACTOR, DIMENSIONS, TOURN_SIZE, MUT_INDPB, DISTANCE_WEIGHT, ANGLE_WEIGHT, STABILITY_WEIGHT)
 
 toolbox = base.Toolbox()
-creator.create("FitnessCompound", base.Fitness, weights=(1.0, -0.5, -0.5))
+creator.create("FitnessCompound", base.Fitness, weights=(DISTANCE_WEIGHT, ANGLE_WEIGHT, STABILITY_WEIGHT, HEIGHT_WEIGHT))
 creator.create("Individual", list, fitness=creator.FitnessCompound)
+
+# creator.create("FitnessSingle", base.Fitness, weights=(DISTANCE_WEIGHT,))
+# creator.create("Individual", list, fitness=creator.FitnessSingle)
 
 for i in range(DIMENSIONS):
     toolbox.register("layer_size_attribute_" + str(i), random.uniform, BOUNDS_LOW[i], BOUNDS_HIGH[i])
@@ -39,16 +51,15 @@ for i in range(DIMENSIONS):
 toolbox.register("individualCreator", tools.initCycle, creator.Individual, layer_size_attributes, n=1)
 toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
 toolbox.register("evaluate", h.evaluateGait)
-toolbox.register("select", tools.selTournament, tournsize=TOURN_SIZE)
-# toolbox.register("select", tools.selStochasticUniversalSampling)
+# toolbox.register("select", tools.selTournament, tournsize=TOURN_SIZE)
+toolbox.register("select", tools.selNSGA2)
 toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUNDS_LOW, up=BOUNDS_HIGH, eta=CROWDING_FACTOR)
-# toolbox.register("mate", tools.cxUniform, indpb=0.5)
-# toolbox.register("mate", tools.cxBlend, alpha=1.0)
 toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUNDS_LOW, up=BOUNDS_HIGH, eta=CROWDING_FACTOR, indpb=MUT_INDPB)
 
 
 # Genetic Algorithm flow:
 def main():
+    lastTime = time.time()
     pool = multiprocessing.Pool()
     toolbox.register("map", pool.map)
     population = toolbox.populationCreator(n=POPULATION_SIZE)
@@ -63,6 +74,7 @@ def main():
     print("-- Best Fitness = ", best.fitness.values[0])
     maxFitnessValues, meanFitnessValues = logbook.select("max", "avg")
     pool.close()
+    print(f"Total Time Elapsed: {((time.time() - lastTime) / 60.0)} minutes")
 
     # plot statistics:
     sns.set_style("whitegrid")
